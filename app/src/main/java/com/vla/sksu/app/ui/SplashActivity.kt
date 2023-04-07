c46fcc069151b8701ea76d389cb04ed98da1d1fe
Philip Borbon <philipjohnborbon@gmail.com>
2023-04-07 23:04:57 +0800

    App splash screen

--- app/src/main/java/com/vla/sksu/app/ui/SplashActivity.kt
+++ app/src/main/java/com/vla/sksu/app/ui/SplashActivity.kt
@@ -1,12 +1,13 @@
 package com.vla.sksu.app.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vla.sksu.app.manager.AuthorizationStore
 
-
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var authorizationStore: AuthorizationStore
