3b3b2d664a45114cee7ca14c48030b44518fd5fc
Philip Borbon <philipjohnborbon@gmail.com>
2023-04-17 20:13:32 +0800

    Show version number in about

--- app/src/main/java/com/vla/sksu/app/ui/about/AboutFragment.kt
+++ app/src/main/java/com/vla/sksu/app/ui/about/AboutFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.vla.sksu.app.BuildConfig
 import com.vla.sksu.app.R
 
 class AboutFragment : Fragment() {
@@ -12,7 +14,11 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val textVersion: TextView = view.findViewById(R.id.version)
+        textVersion.text = getString(R.string.text_version, BuildConfig.VERSION_NAME)
+
+        return view
     }
 }
